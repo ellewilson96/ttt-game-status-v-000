fefd9fd71fc2e39bb699ac22bad3460b2f434f81
Elle Wilson <eleanorwilsonn@gmail.com>
2018-01-24 18:49:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     win_combination.each do |win_index|
-      
+
       position_1 = board[win_index_1]
       puts win_index_1
       position_2 = board[win_index_2]
