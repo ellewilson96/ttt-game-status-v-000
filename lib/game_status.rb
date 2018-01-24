4b01811564c9311dca0d3c91317fefe2b0c107a2
Elle Wilson <eleanorwilsonn@gmail.com>
2018-01-24 19:16:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,7 +8,6 @@ WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    win_array = []
       win_index_1 = win_combination[0]
       win_index_2 = win_combination[1]
       win_index_3 = win_combination[2]
@@ -17,8 +16,7 @@ def won?(board)
       position_2 = board[win_index_2]
       position_3 = board[win_index_3]
 
-      if position_1 == "X" && position_2 == "X" && position_3 == "X"
-        win_array << win_combination
+      win_combination.select if position_1 == "X" && position_2 == "X" && position_3 == "X"
       else
         false
       end
