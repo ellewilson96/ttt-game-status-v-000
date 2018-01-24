bb4fbdea02c45a45fde5f85fd9327340016d1914
Elle Wilson <eleanorwilsonn@gmail.com>
2018-01-24 19:26:59 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,7 +8,7 @@ WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8
 
 def won?(board)
 WIN_COMBINATIONS.find do |win_combo|
-  board[win_combo[0]] == board[win_combo[1]] && board[win_combo[0]] == board[win_combo[2]] 
+  board[win_combo[0]] == board[win_combo[1]] && board[win_combo[0]] == board[win_combo[2]]
   && position_taken?(board, win_combo[1])
 end
 end
