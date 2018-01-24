b4b4213d144f7a3b922674f739f4abe1f0612246
Elle Wilson <eleanorwilsonn@gmail.com>
2018-01-24 19:53:33 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ def full?(board)
 end
 
 def draw?(board)
-  if won?(board) == false && full?(board) == true
+  if won?(board) && full?(board) == true
     true
   elsif full?(board) == false && won?(board) == false
     false
